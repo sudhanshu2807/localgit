779a0697c79c41523789eb2d58578d285a4dd481
sudhanshu2807 <sudhanshuraj8651475233@gmail.com>
2021-01-17 00:30:09 +0530

    multiplicationbyuser
    git commit -m multiplicationbyuser

---
+++ multi2.c
@@ -0,0 +1,14 @@
+//multiplication in C
+#include<stdio.h>
+int main()
+{ int a,b,result;
+  result = a*b;
+  printf("enter the value of a is %d\n",a);
+  scanf("%d",&a);
+  printf("enter the value of b is %d\n",b);
+  scanf("%d",&b);
+
+  printf("value of the result is %d\n",a*b);
+  
+return 0;
+}
