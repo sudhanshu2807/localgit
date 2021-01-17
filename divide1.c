975805b93df71aa7ee15870d50b3e8f1eded08cd
sudhanshu2807 <76770192+sudhanshu2807@users.noreply.github.com>
2021-01-18 03:31:17 +0530

    Add files via upload

---
+++ divide1.c
@@ -0,0 +1,18 @@
+//division
+#include<stdio.h>
+
+int main()
+{
+ int a,b,result;
+ 
+ printf("enter the value of a is %d\n",a);
+ scanf("%d",&a);
+ printf(" enter the value of b is %d\n",b);
+ scanf("%d",&b);
+ result = a/b;
+ printf("display the result is %d\n",result);
+  
+ 
+
+return 0;
+}
