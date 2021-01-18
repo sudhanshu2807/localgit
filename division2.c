039785e64c1d22b4b5ec1ec8d86c36afd9b98d68
sudhanshu2807 <sudhanshuraj8651475233@gmail.com>
2021-01-18 11:36:41 +0530

    monday work

---
+++ division2.c
@@ -0,0 +1,16 @@
+//division
+#include<stdio.h>
+
+int main()
+{
+ int a,b,result;
+ 
+ a = 2;
+ b = 2;
+ result = a/b;    //a divided by b
+ printf("value after division is %d",result);
+ 
+
+
+return 0;
+}
